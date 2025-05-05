e16e1ae7770d5c24057a11535e60a4a346921fd3
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-05-05 20:23:18 +0200

    add CategoriesComponent with fetch and delete functionality

--- FrontEnd/src/app/categories/categories.component.ts
+++ FrontEnd/src/app/categories/categories.component.ts
@@ -1,14 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent implements OnInit {
-  ngOnInit(): void {
-    console.log("Works")
+  categories: any[] = [];
+  show(): void{
+    fetch("https://localhost:7165/Category")
+      .then(response => response.json())
+      .then(parsedResponse => this.categories = parsedResponse)
   }
-
+  ngOnInit(): void {
+    this.show();
+  }
+  delete(id: string): void {
+    const options = {
+      method: "DELETE"
+    }
+    fetch("https://localhost:7165/Category/"+id, options)
+    .then(Response=>Response.json())
+    .then(parsedResponse=>{
+      if(parsedResponse.success){
+          alert("Törölve")
+          this.show();
+      }
+      else{
+        alert("Sikertelen törlés")
+      }
+    })
+  }
 }
