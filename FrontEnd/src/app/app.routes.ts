e661e810f5d08691dbcf15a5a9a8a6078b068304
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-05-05 20:30:12 +0200

    configure application routes with fallback to NotFoundComponent

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { CategoryModifyComponent } from './category-modify/category-modify.component';
+import { DiagramsComponent } from './diagrams/diagrams.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {path:"categories",component:CategoriesComponent},
-    {path:"",component:AppComponent}
+    {path:"",component:MainPageComponent},
+    {path:"recipes", component:RecepiesComponent},
+    {path:"category-modify/:id", component:CategoryModifyComponent},
+    {path:"diagrams", component:DiagramsComponent},
+    {path:"**", component:NotFoundComponent}
+
 ];
