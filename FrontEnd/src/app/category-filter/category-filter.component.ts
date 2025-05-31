a4354df5a0356c80046cd98edd2902f3e8a8abd4
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-05-31 14:59:54 +0200

    Refactor category filter to use RecipeService and name param

--- FrontEnd/src/app/category-filter/category-filter.component.ts
+++ FrontEnd/src/app/category-filter/category-filter.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../services/recipe.service';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category-filter',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './category-filter.component.html',
   styleUrl: './category-filter.component.css'
 })
 export class CategoryFilterComponent implements OnInit {
-  categoryId: string="";
-  category: any []=[];
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute){}
+  categoryName: string = "";
+  recipes: any[] = [];
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params=>this.categoryId=params.get("id")+"")
-    fetch("https://localhost:7165/Category/"+this.categoryId)
-    .then(response=>response.json())
-    .then(parsed=>this.category=parsed)
+    this.route.paramMap.subscribe(params => this.categoryName = params.get("name") + "")
+    this.recipeService.getRecipesByCategory(this.categoryName)
+      .subscribe(resp => this.recipes = resp.recipes)
   }
 
 }
