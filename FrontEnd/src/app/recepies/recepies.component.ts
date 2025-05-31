720a9a68579a8e6737dc638bf03d22f0d6536805
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-05-31 15:09:07 +0200

    Replace fetch with RecipeService in Recipe component

--- FrontEnd/src/app/recepies/recepies.component.ts
+++ FrontEnd/src/app/recepies/recepies.component.ts
@@ -1,19 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-recepies',
-  imports: [CommonModule],
+  imports: [CommonModule
+  ],
   templateUrl: './recepies.component.html',
   styleUrl: './recepies.component.css'
 })
 export class RecepiesComponent implements OnInit {
-  recipes:any[]=[];
+  recipes: any[] = [];
+
+  constructor(private recipeService: RecipeService) {
+
+  }
   ngOnInit(): void {
- fetch("https://localhost:7165/Recipe")
- .then(response=>response.json())
- .then(response=>this.recipes=response);
-}
+    this.recipeService.getAllRecipes()
+      .subscribe(resp => this.recipes = resp)
+  }
 
 
 }
