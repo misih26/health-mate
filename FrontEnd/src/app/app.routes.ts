96a5cb38ee5f52bcbc020ca932743c6b4daafa1a
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-05-31 14:57:57 +0200

    Add new routes and update existing paths

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -6,13 +6,19 @@ import { RecepiesComponent } from './recepies/recepies.component';
 import { CategoryModifyComponent } from './category-modify/category-modify.component';
 import { DiagramsComponent } from './diagrams/diagrams.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CategoryFilterComponent } from './category-filter/category-filter.component';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
+import { NewCategoryComponent } from './new-category/new-category.component';
 
 export const routes: Routes = [
-    {path:"categories",component:CategoriesComponent},
-    {path:"",component:MainPageComponent},
-    {path:"recipes", component:RecepiesComponent},
-    {path:"category-modify/:id", component:CategoryModifyComponent},
-    {path:"diagrams", component:DiagramsComponent},
-    {path:"**", component:NotFoundComponent}
+    { path: "categories", component: CategoriesComponent },
+    { path: "", component: MainPageComponent },
+    { path: "recipes", component: RecepiesComponent },
+    { path: "category-modify/:id", component: CategoryModifyComponent },
+    { path: "diagrams", component: DiagramsComponent },
+    { path: "category-by-name/:name", component: CategoryFilterComponent },
+    { path: "new-recipe", component: NewRecipeComponent },
+    { path: "new-category", component: NewCategoryComponent },
+    { path: "**", component: NotFoundComponent }
 
 ];
