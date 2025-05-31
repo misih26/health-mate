ef3a3fec770ef66c9d0d16432610c35f45a63cf9
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-05-31 15:03:58 +0200

    code formatting and greeting text added for main page

--- FrontEnd/src/app/main-page/main-page.component.ts
+++ FrontEnd/src/app/main-page/main-page.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-
+  appDescription = `
+    A HealthMate egy egészségtudatos receptkezelő alkalmazás, ahol recepteket és kategóriákat hozhatsz létre, módosíthatsz vagy törölhetsz.
+    Kategóriánként szűrve is válogathatsz számos egészséges recept közül , és egy diagram segítségével láthatod, hogy melyik kategóriában hány recept található meg.
+  `;
 }
