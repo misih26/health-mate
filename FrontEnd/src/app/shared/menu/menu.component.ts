444e595f48df790022c5a4154e90d1a3d5d61ae5
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-12-29 00:48:36 +0100

    fix routing

--- FrontEnd/src/app/shared/menu/menu.component.ts
+++ FrontEnd/src/app/shared/menu/menu.component.ts
@@ -3,7 +3,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
